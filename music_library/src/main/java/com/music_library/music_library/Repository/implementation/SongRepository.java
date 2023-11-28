6179daa48d0401bf94d29fb09d9c6ffa05b8aead
varag5 <varhidy.agoston@gmail.com>
2023-11-28 16:57:05 +0100

    Solution

---
+++ music_library/src/main/java/com/music_library/music_library/Repository/implementation/SongRepository.java
@@ -0,0 +1,32 @@
+package com.music_library.music_library.Repository.implementation;
+
+import com.music_library.music_library.Repository.abstraction.ISongRepository;
+import com.music_library.music_library.domain.Song;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class SongRepository implements ISongRepository {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Override
+    public List<Song> getAllSongs() {
+        try {
+            return entityManager.createQuery("SELECT s FROM Song s", Song.class).getResultList();
+        } catch (Exception e) {
+            log.error("Error fetching all songs", e);
+            throw new RuntimeException("Error fetching all songs", e);
+        }
+    }
+}
